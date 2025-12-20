ccd1cd57f915e2aa8f9a512c266d5b93764d1f47
Prince PM <pmprince2024@gmail.com>
2025-12-20 14:29:59 +0530

    Fix homepage route for Render deployment

--- server.js
+++ server.js
@@ -83,6 +83,14 @@ app.get("/create-random-user", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+const path = require("path");
+
+// Serve index.html for root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
