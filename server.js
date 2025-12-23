2712c65071a3e0e696b4beff149b25c1337f5256
Prince PM <pmprince2024@gmail.com>
2025-12-23 11:28:02 +0530

    Use MongoDB Atlas and dynamic port

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/instaDB")
+//mongoose.connect("mongodb://127.0.0.1:27017/instaDB")
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
@@ -75,6 +77,13 @@ app.post("/login", async (req, res) => {
 
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+//app.listen(5000, () => {
+ // console.log("Server running on port 5000");
+
+ const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
 });
